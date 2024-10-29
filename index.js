099bd1db6de6ff2213d1472f14c897c254791760
Marcospimentaadv <marcoskraker21@gmail.com>
2024-10-29 16:40:56 -0300

    Classificação Nivel de Herói

---
+++ index.js
@@ -0,0 +1,16 @@
+let quantidadeDeXP = 0
+for(let i = 0 ; i < 1 ; i++) {
+    quantidadeDeXP = "10001 pontos"
+
+   
+console.log("Está no nivel Radiante " );
+
+}
+console.log( quantidadeDeXP + " Do Super Geforce RTX ")
+
+
+
+
+
+
+
